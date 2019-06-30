7f8353631096bbbfa879dd8dc8325c28e1e18414
You-hyeonyeong <dbgusdud412@naver.com>
2019-06-30 23:19:35 +0900

    setting

--- routes/archive/index.js
+++ routes/archive/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/signin', require('./signin'));
-router.use('/signup', require('./signup'));
+router.use('/', require('./test'));
 
 module.exports = router;
