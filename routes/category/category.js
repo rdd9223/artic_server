e49f466a72e7d37420e6209d4ba3eb682f1a92ed
YeongWoo Kang <rdd9223@naver.com>
2019-07-02 15:04:43 +0900

    category edit

--- routes/category/category.js
+++ routes/category/category.js
@@ -4,16 +4,17 @@ const defaultRes = require('../../modules/utils/utils');
 const statusCode = require('../../modules/utils/statusCode');
 const resMessage = require('../../modules/utils/responseMessage');
 const db = require('../../modules/pool');
-const upload = require('../../config/multer');
 
 // 카테고리 전체조회
 router.get('/', async(req,res)=>{
 	const getAllCategoryQuery = 'SELECT * FROM category';
-	const getAllcategoryResult = await db.queryParam_None(getAllCategoryQuery);
+	const getAllCategoryResult = await db.queryParam_None(getAllCategoryQuery);
 
-	if(!getAllcategoryResult){
+	if(!getAllCategoryResult){
 		res.status(200).send(defaultRes.successFalse(statusCode.CATEGORY_SELECT_FAIL));
 	} else {
-		res.status(200).send(defaultRes.successTrue(statusCode.CATEGORY_SELECT_SUCCESS, getAllcategoryResult));
+		res.status(200).send(defaultRes.successTrue(statusCode.CATEGORY_SELECT_SUCCESS, getAllCategoryResult));
 	}
 })
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/auth', require('./auth/index'));
+//router.use('/auth', require('./auth/index'));
 router.use('/category', require('./category/index'));
 
 module.exports = router;
