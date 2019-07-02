63df1935df6b5a172cafa0f20958abe4676b23cd
YeongWoo Kang <rdd9223@naver.com>
2019-07-02 14:32:31 +0900

    config add

--- modules/utils/responseMessage.js
+++ modules/utils/responseMessage.js
@@ -2,14 +2,8 @@ module.exports = {
     NULL_VALUE: "필요한 값이 없습니다",
     OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
-    MEMBERSHIP_SELECT_SUCCESS: "멤버십 조회 성공",
-    MEMBERSHIP_SELECT_FAIL: "멤버십 조회 실패",
-    MEMBERSHIP_INSERT_SUCCESS: "멤버십 삽입 성공",
-    MEMBERSHIP_INSERT_FAIL: "멤버십 삽입 실패",
-    MEMBERSHIP_UPDATE_SUCCESS: "멤버십 수정 성공",
-    MEMBERSHIP_UPDATE_FAIL: "멤버십 수정 실패",
-    MEMBERSHIP_TRANSAC_SUCCESS: "멤버십 트렌젝션 성공",
-    MEMBERSHIP_TRANSAC_FAIL: "멤버십 트렌젝션 실패",
+	CATEGORY_SELECT_SUCCESS: "카테고리 조회 성공",
+    CATEGORY_SELECT_FAIL: "카테고리 조회 실패",
 
     WEBTOON_LIST_ALL_SELECT_SUCCESS: "웹툰 전체 조회 성공",
     WEBTOON_LIST_ALL_SELECT_FAIL: "웹툰 전체 조회 실패",
