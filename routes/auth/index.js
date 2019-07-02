4057dd294b452c51825996076ff62d2a9b3a3aed
YeongWoo Kang <rdd9223@naver.com>
2019-07-02 13:37:27 +0900

    category setting

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,5 +3,5 @@ var router = express.Router();
 
 // router.use('/signin', require('./signin'));
 // router.use('/signup', require('./signup'));
-
+// router.use('/category', require('./category));
 module.exports = router;
