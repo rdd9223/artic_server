0962c4b5cf741a6783d738250053c8d09108b788
You-hyeonyeong <dbgusdud412@naver.com>
2019-07-06 15:40:35 +0900

    home edit com

--- routes/home/archive.js
+++ routes/home/archive.js
@@ -4,32 +4,45 @@ const defaultRes = require('../../modules/utils/utils');
 const statusCode = require('../../modules/utils/statusCode');
 const resMessage = require('../../modules/utils/responseMessage');
 const db = require('../../modules/pool');
+const authUtils = require('../../modules/utils/authUtils');
 const upload = require('../../config/multer');
 
-// 신규 아카이브 더보기
-router.get('/new', async (req, res) => {
-	const getNewArchiveQuery = 'SELECT ar.*, ca.category_title FROM archive ar INNER JOIN category ca where ar.category_idx = ca.category_idx';
+// 신규 아카이브 더보기 (최신순 정렬)
+router.get('/new', authUtils.isLoggedin, async (req, res) => {
+	const user_idx = req.decoded.idx;
+	const getNewArchiveQuery = 'SELECT ca.category_title, ar.*  FROM archive ar INNER JOIN category ca where ar.category_idx = ca.category_idx ORDER BY date DESC';
 	const getNewArchiveResult = await db.queryParam_None(getNewArchiveQuery);
+	const getNewArticleCount = 'SELECT count(article_idx) count FROM archiveArticle WHERE archive_idx = ? '; //해당 아카이브에 들어있는 아티클개수
 
 	if (!getNewArchiveResult) {
 		res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.HOME_NEW_FAIL));
 	} else {
+		for(var i = 0, archive; archive = getNewArchiveResult[i]; i++) {
+			const archiveIdx = archive.archive_idx;
+			const archiveCount = await db.queryParam_Arr(getNewArticleCount,[archiveIdx])
+			archive.article_cnt = archiveCount[0].count
+		}
 		res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.HOME_NEW_SUCCESS, getNewArchiveResult));
 	}
 });
 // 신규 아카이브 하나보기
-//아카이브 스크랩 여부, 아티클 좋아요/담기여부 만 추가하면됨
-router.get('/new/:archive_idx', async (req, res) => {
+router.get('/new/:archive_idx', authUtils.isLoggedin, async (req, res) => {
+	const user_idx = req.decoded.idx;
 	const idx = req.params.archive_idx;
-
 	const getOneNewArchiveQuery = 'SELECT ac.archive_title, ar.article_idx, at.* FROM archive ac, archiveArticle ar, article at WHERE ac.archive_idx = ar.archive_idx AND ar.article_idx = at.article_idx' ;
 	const getOneConntArticle = 'SELECT * FROM article WHERE article_idx = ?';
+	const getNewArchiveScrap = 'SELECT count(*) scrap FROM archiveAdd WHERE user_idx = ? AND archive_idx = ? ';//해당 아카이브 스크랩 여부
 	const getOneNewArchiveResult = await db.queryParam_None(getOneNewArchiveQuery);
 
 	console.log(getOneNewArchiveResult);
 	if (!getOneNewArchiveResult) {
 		res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.HOME_NEW_FAIL));
 	} else {
+		for(var i = 0, archive; archive = getNewArchiveResult[i]; i++) {
+			const archiveIdx = archive.archive_idx;
+			const archiveScrap = await db.queryParam_Parse(getNewArchiveScrap,[user_idx, archiveIdx])
+			archive.article_scarp = archiveScrap[0].scrap
+		}
 		res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.HOME_NEW_SUCCESS, getOneNewArchiveResult));
 	}
 });
