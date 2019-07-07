e1804178e1829ce9201b69d64e59161a2425888e
You-hyeonyeong <dbgusdud412@naver.com>
2019-07-08 06:49:27 +0900

    아이디 중복 검사, 신규 아티클 조회

--- routes/home/article.js
+++ routes/home/article.js
@@ -8,7 +8,7 @@ const authUtils = require('../../modules/utils/authUtils');
 const upload = require('../../config/multer');
 
 // 신규 아티클 더보기
-router.get('/articles/new', async (req, res) => {
+router.get('/articles/new',authUtils.isLoggedin, async (req, res) => {
 	const getNewArticleQuery = 'SELECT * FROM article ORDER BY date DESC';
 	const getNewArticleResult = await db.queryParam_None(getNewArticleQuery);
 
@@ -19,27 +19,25 @@ router.get('/articles/new', async (req, res) => {
 	}
 
 });
-// 신규 아티클 하나 다시
-router.get('/:article_idx', async (req, res) => {
+// 신규 아티클 하나 
+router.get('/new/:article_idx', authUtils.isLoggedin, async (req, res) => {
 	const idx = req.params.article_idx;
+	//const user_idx = req.decoded.idx;
 	const getOneNewArticleQuery = 'SELECT * FROM artic.article WHERE article_idx = ?'
 	const getOneNewArticleResult = await db.queryParam_Parse(getOneNewArticleQuery, [idx]);
+	console.log(`이건되는디${getOneNewArticleResult[0].article_idx}`)
 	if (!getOneNewArticleResult) {
 		res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.HOME_NEW_FAIL));
 	} else {
-		// for (var i = 0, article; article = getOneNewArticleResult[i]; i++) {
-		// 	const articleIdx = article.archive_idx;
-		const getOneNewArchive = 'SELECT archive_idx FROM artic.archiveArticle WHERE article_idx = ? ORDER BY archiveArticle_idx LIMIT 1' //그 아티클이 속해있는 아카이브 제일 처음꺼
+		const getOneNewArchive = 'SELECT * FROM artic.archiveArticle WHERE article_idx = ? ORDER BY archiveArticle_idx LIMIT 1'; //그 아티클이 속해있는 아카이브 제일 처음꺼
 		const archiveTitleIdx = await db.queryParam_Arr(getOneNewArchive, [idx]);
-		console.log(`dddddddddddd${archiveTitleIdx}`)
+		console.log(`해당 아티클이 속해있는 아카이브 인덱스1${archiveTitleIdx[0].archive_idx}`)
 		const getOneNewArchiveTitle = 'SELECT archive_title FROM artic.archive WHERE archive_idx = ?'
 		const archiveTitle = await db.queryParam_Arr(getOneNewArchiveTitle, [archiveTitleIdx[0].archive_idx])
-		console.log(`ffffffffffff${archiveTitle}`)
-		// 	article.archive_title = archiveTitle[0].archive_title;
-		// }
-
-		getOneNewArticleResult.archive_title = archiveTitle[0].archive_title;
-		res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.HOME_NEW_SUCCESS, getOneNewArticleResult));
+		console.log(`ffffffffffff${archiveTitle[0].archive_title}`)
+		getOneNewArticleResult[0].archive_title = archiveTitle[0].archive_title;
+		//getOneNewArticleResult.archive_title = archiveTitle.archive_title;
+		res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.HOME_NEW_SUCCESS, getOneNewArticleResult[0]));
 	}
 });
 //최근 읽은 아티클
@@ -61,7 +59,7 @@ router.get('/history', authUtils.isLoggedin, async (req, res) => {
 	}
 });
 
-//아틱의 선택 
+//아틱의 추천
 router.get('/pick', async (req, res) => {
 	const getArticlePickQuery = 'SELECT * FROM artic.article WHERE pick = 0';
 	const getArticlePickResult = await db.queryParam_None(getArticlePickQuery);
