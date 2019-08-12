6c927e0a131b4ff68b6bbb033d52a9c45a47ce82
You-hyeonyeong <dbgusdud412@naver.com>
2019-08-12 22:06:19 +0900

    도메인 크롤러 수정

--- modules/crawling/dbconfig.py
+++ modules/crawling/dbconfig.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import sys
 import io
+import tldextract
+
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 # 403에러시 헤더처리 하기
-#headers = {'User-Agent': 'Mozilla/5.0'}   , headers=headers
-res = requests.get(sys.argv[1])
+headers = {'User-Agent': 'Mozilla/5.0'}  
+res = requests.get(sys.argv[1] , headers=headers)
 link = sys.argv[1]
 soup = BeautifulSoup(res.content, 'html.parser')
 title = str(soup.find('title').get_text()).strip()
@@ -22,8 +24,10 @@ except TypeError:
 # YYYY/mm/dd HH:MM:SS 형태의 시간 출력
 date = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
 
-tmp = link.split('/')
-domain = tmp[2]
+#tmp = link.split('/')
+#domain = tmp[2]
+extracted = tldextract.extract(link)
+domain = "{}.{}".format(extracted.domain, extracted.suffix)
 
 print(title)
 print(output)
